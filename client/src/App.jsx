82397a4c5e57863a44c54435ddcbcd217578fb76
swapyshelke <swapyshelke02gmail.com>
2024-06-23 15:34:55 +0530

    setup completed & router added

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+
+function App() {
+  return <>
+  <h1>app component</h1></>;
+}
+
+export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Profile from './pages/Profile.jsx';
+import Signout from './pages/Signout.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },{
+    path: "/signout",
+    element: <Signout />
+  }
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}
+/>
+)
