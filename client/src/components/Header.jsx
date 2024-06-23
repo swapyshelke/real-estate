94b39f4539809a7294326ceea8f0a1a8f1e3cdc3
swapyshelke <swapyshelke02gmail.com>
2024-06-23 16:05:09 +0530

    CSS and navbar added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+// import "../App.css"
 
 const Header = () => {
   return (
-    <div>Header - common section</div>
+    <header>
+    <nav>
+        <div class="logo">Event Organizer</div>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Who we are</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">Signup</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+    </nav>
+</header>
   )
 }
 
